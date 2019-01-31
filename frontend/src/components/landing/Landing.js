bee7f65239a4c06333f93d229a9431b8f406cbc6
fajfara <a.fajfar95@gmail.com>
2019-01-31 12:50:28 +0100

    New version of frontend, removed and changed a lot of stuff

---
+++ frontend/src/components/landing/Landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="landing-content">
+        <h1 className="landing-content--title">Schoolab</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
