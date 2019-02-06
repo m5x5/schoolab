9eb97a175c6773b2a55f85c9a747bb0b781c2b98
Michael55555 <michaelpeters5647@gmail.com>
2019-02-06 23:45:24 +0100

    Flashcard trainer

--- frontend/src/components/Flashcards.vue
+++ frontend/src/components/Flashcards.vue
@@ -5,20 +5,15 @@
     <button @click="remove">Remove</button>
     <p>Score: {{score}}</p>
     <h2 id="card-title">{{title}}</h2>
-    <p id="card-text">{{input}}</p>
-    <input type="text" v-model="input">
-    <button @click="check">Submit</button>
+    <form>
+      <input type="text" v-model="input">
+      <button @click="check">Submit</button>
+    </form>
   </div>
 </div>
 </template>
 
 <script type="text/javascript">
-let model = {
-  spanishVocab: {
-    spanish: ['die Lektion', "un", "l\'entrée", 'le texte', 'l\'atelier', 'sur place'],
-    german: ['die Lektion', 'eins', 'der Eingang', 'der Text', 'die Werkstatt', 'vor Ort'],
-  },
-}
 // Simulated Mongodb flashcard object
 let flashcards = {
   'animals': [
@@ -31,22 +26,24 @@ let flashcards = {
   ]
 }
 
+const group = 'animals'
+
 export default {
   name: 'Flashcards',
   data() {
     return {
       groups: Object.keys(flashcards),
       input: '',
-      title: model.spanishVocab.german[0],
+      title: flashcards[group][0][0],
       coins: 0,
       score: 0,
       count: 0
     }
   },
   methods: {
-    check() {
-      console.log(this.input)
-      if (this.input == model.spanishVocab.spanish[this.count]) {
+    check(e) {
+      e.preventDefault();
+      if (this.input === flashcards[group][this.count][1]) {
         alert('true');
         this.input = '';
         this.count++;
@@ -54,30 +51,31 @@ export default {
       } else {
         this.input = '';
         alert('false');
-        alert('the correct answer was: ' + model.spanishVocab.spanish[this.count]);
+        alert('the correct answer was: ' + flashcards[group][this.count][1]);
         this.coins--;
       }
-      if (model.count > model.spanishVocab.german.length - 1) {
-        model.count = 0;
+      if (this.count === flashcards[group].length) {
+        this.count = 0;
       }
+      this.render();
+
     },
-    remove () {
-      model.spanishVocab.spanish.splice(this.count, 1);
-      model.spanishVocab.german.splice(this.count, 1);
+    remove() {
+      flashcards[group].splice(this.count, 1);
+      flashcards[group].splice(this.count, 1);
       this.count++;
       this.render();
     },
-    init () {
+    init() {
       this.render();
     },
 
     render() {
       this.score = this.coins;
-      this.title = model.spanishVocab.german[model.count];
+      this.title = flashcards[group][this.count][0]
     }
   }
 }
-
 </script>
 
 <style media="screen">
@@ -86,11 +84,16 @@ export default {
   padding: 10px;
   border-radius: 10px;
   font-size: 36px;
+  font-weight: 400;
 }
 
 p {
   margin: 0;
   text-align: right;
   font-size: 2vmin;
+}
+
+h2 {
+  font-weight: 400;
 }
 </style>
